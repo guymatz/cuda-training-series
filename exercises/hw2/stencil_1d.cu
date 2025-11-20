1fa1660355d76e0e63fc79d68da720daaaa80e66
Guy Matz <guyeric.matz@studenti.unipd.it>
2025-11-20 17:42:29 +0100

    why is this not getting past __syncthreads?!

--- exercises/hw2/stencil_1d.cu
+++ exercises/hw2/stencil_1d.cu
@@ -1,38 +1,52 @@
 #include <stdio.h>
 #include <algorithm>
+#include <math.h>
+#include "utils.cuh"
 
 using namespace std;
 
-#define N 4096
-#define RADIUS 3
+//#define N 1024
+#define N 16
+#define RADIUS 1
 #define BLOCK_SIZE 16
 
 __global__ void stencil_1d(int *in, int *out) {
-    __shared__ int temp[FIXME];
+    __shared__ int temp[2 * RADIUS + 1];
     int gindex = threadIdx.x + blockIdx.x * blockDim.x;
-    int lindex = FIXME;
+    int lindex = threadIdx.x + RADIUS;
+	printf("Here %i %i %i %i\n", gindex, lindex, threadIdx.x, RADIUS);
 
     // Read input elements into shared memory
+	printf("Setting temp[%i] to in[%i] = %i\n", lindex, gindex, in[gindex]);
     temp[lindex] = in[gindex];
+	printf("before IF\n");
     if (threadIdx.x < RADIUS) {
+	  printf("Setting %i to %i\n", lindex-RADIUS, in[gindex-RADIUS]);;
       temp[lindex - RADIUS] = in[gindex - RADIUS];
+	  printf("Setting %i to %i\n", lindex+BLOCK_SIZE, in[gindex+BLOCK_SIZE]);;
       temp[lindex + BLOCK_SIZE] = in[gindex + BLOCK_SIZE];
     }
 
     // Synchronize (ensure all the data is available)
     __syncthreads();
+	printf("AFTER syncthreads\n");
 
     // Apply the stencil
     int result = 0;
-    for (int offset = -RADIUS; offset <= RADIUS; offset++)
-      result += temp[FIXME];
+    for (int offset = -RADIUS; offset <= RADIUS; offset++) {
+		printf("here: %i\n", temp[offset]);
+      result += temp[offset];
+	}
 
     // Store the result
     out[gindex] = result;
 }
 
 void fill_ints(int *x, int n) {
-  fill_n(x, n, 1);
+  //fill_n(x, n, 1);
+  for (int i = 0; i < n; i++) {
+	  x[i] = 1;
+  }
 }
 
 int main(void) {
@@ -40,9 +54,13 @@ int main(void) {
   int *d_in, *d_out; // device copies of a, b, c
 
   // Alloc space for host copies and setup values
-  int size = (FIXME) * sizeof(int);
+  int size = (N + 2*RADIUS) * sizeof(int);
   in = (int *)malloc(size); fill_ints(in, N + 2*RADIUS);
   out = (int *)malloc(size); fill_ints(out, N + 2*RADIUS);
+  //printf("in:\n");
+  //print_matrix(in, (N+2*RADIUS));
+  //printf("out before:\n");
+  //print_matrix(out, (N+2*RADIUS));
 
   // Alloc space for device copies
   cudaMalloc((void **)&d_in, size);
@@ -53,10 +71,14 @@ int main(void) {
   cudaMemcpy(d_out, out, size, cudaMemcpyHostToDevice);
 
   // Launch stencil_1d() kernel on GPU
-  stencil_1d<<<N/BLOCK_SIZE,BLOCK_SIZE>>>(FIXME, FIXME);
+  stencil_1d<<<ceil((float)N/BLOCK_SIZE),BLOCK_SIZE>>>(in, out);
 
   // Copy result back to host
   cudaMemcpy(out, d_out, size, cudaMemcpyDeviceToHost);
+  //printf("out after:\n");
+  //print_matrix(out, N);
+  /*
+  return 1;
 
   // Error Checking
   for (int i = 0; i < N + 2*RADIUS; i++) {
@@ -68,7 +90,7 @@ int main(void) {
     	printf("Mismatch at index %d, was: %d, should be: %d\n", i, out[i], 1 + 2*RADIUS);
     }
   }
-
+ */
   // Cleanup
   free(in); free(out);
   cudaFree(d_in); cudaFree(d_out);
