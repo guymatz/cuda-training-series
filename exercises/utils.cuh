747f2647537fd9b9d316f46e50576206751cea01
Guy Matz <guyeric.matz@studenti.unipd.it>
2025-11-23 14:47:36 +0100

    added print matrix functions for int and float. if len of matrix < 100 then we print the whole thing

--- exercises/utils.cuh
+++ exercises/utils.cuh
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 
 inline cudaError_t checkCuda(cudaError_t result) {
 	if (result != cudaSuccess) {
@@ -9,7 +10,16 @@ inline cudaError_t checkCuda(cudaError_t result) {
 	return result;
 }
 
-void print_matrix(float* M, int len, int lines=10) {
+void print_fmatrix(float* M, int len, int lines=10) {
+	if (len < 100) {
+		for (int i = 0; i < len ; i++) {
+				if (i % lines == 0) printf("\n");
+				printf("%4.2f ", M[i]);
+		}
+		printf("\n");
+		return;
+	}
+
 		for (int i = 0; i < lines * lines; i++) {
 				if (i % lines == 0) printf("\n");
 				printf("%4.2f ", M[i]);
@@ -25,6 +35,34 @@ void print_matrix(float* M, int len, int lines=10) {
 		for (int i = (len - lines * lines); i < len; i++) {
 				if (i % lines == 0) printf("\n\t\t\t\t\t\t\t\t");
 				printf("%4.2f ", M[i]);
+		}
+		printf("\n");
+}
+
+void print_imatrix(int* M, int len, int lines=10) {
+	if (len < 100) {
+		for (int i = 0; i < len ; i++) {
+				if ( (i % (int)ceil(sqrt(len))) == 0 ) printf("\n");
+				printf("%i ", M[i]);
+		}
+		printf("\n");
+		return;
+	}
+		for (int i = 0; i < lines * lines; i++) {
+				if (i % lines == 0) printf("\n");
+				printf("%i ", M[i]);
+		}
+		printf("\n");
+
+		for (int i = (len/2 - lines * lines/2); i < (len/2 + lines * lines/2); i++) {
+				if (i % lines == 0) printf("\n\t\t\t\t");
+				printf("%i ", M[i]);
+		}
+		printf("\n");
+
+		for (int i = (len - lines * lines); i < len; i++) {
+				if (i % lines == 0) printf("\n\t\t\t\t\t\t\t\t");
+				printf("%i ", M[i]);
 		}
 		printf("\n");
 }
